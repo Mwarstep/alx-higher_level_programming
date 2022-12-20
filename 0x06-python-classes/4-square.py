f5624c5f86b58d32d43734d848f43c73e5342478
Mwarstep <mvoi811@gmail.com>
2022-12-20 04:41:03 -0800

    Task Test v.32

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Define a class Square"""
+"""Define a class Square."""
 
 
 class Square:
     """Represent a square."""
 
     def __init__(self, size=0):
-        """Init a new square.
+        """Initialize a new square.
         Args: 
-        size (int): The size of the new Square.
+            size (int): The size of the new Square.
         """
         self.size = size
 
