cfedffccb1c16624aa66b2e89b8ad0fbd5f5c206
Mwarstep <mvoi811@gmail.com>
2022-12-20 04:19:11 -0800

    Task 1 - Test

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
-"""Defines a class Square"""
+"""Define a class Square."""
 
 class Square:
-    """"Represents a square"""
+    """"Represents a square."""
+
     def __init__(self, size):
-        """"Initializing a new square
+        """"Initialize a new square.
         Args:
             size (int): The size of the new square.
             """
