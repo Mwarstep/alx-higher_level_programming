35d048f10437c61a8008302d1bee835b0074839a
Arnold Mwarstep <111042323+Mwarstep@users.noreply.github.com>
2023-01-04 15:25:35 +0300

    Update 0-main.py

--- 0x07-python-test_driven_development/tests/0-main.py
+++ 0x07-python-test_driven_development/tests/0-main.py
@@ -1,4 +1,3 @@
-guillaume@ubuntu:~/0x07$ cat 0-main.py
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
@@ -7,10 +6,10 @@ print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
 try:
-        print(add_integer(4, "School"))
+    print(add_integer(4, "School"))
 except Exception as e:
-        print(e)
-        try:
-                print(add_integer(None))
-        except Exception as e:
-                print(e)
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
