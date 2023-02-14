cb7892cebf56f7e54dc0c21c961838993d64aaf6
Mwarstep <mvoi811@gmail.com>
2023-02-14 05:48:35 -0800

    Task 19 - Correction

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,3 @@
 -- Imports in hbtn_0c_0 database thi s table dump
 -- Will display avg temp in July and Aug
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month=7 OR month=8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
