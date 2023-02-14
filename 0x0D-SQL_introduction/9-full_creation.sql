b4b4d70525f8872b29991c1438fdc8df803d7892
Mwarstep <mvoi811@gmail.com>
2023-02-14 04:51:38 -0800

    Task 9 - Correction

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Will create a second table
-CREATE TABLE IF NOT EXISTS second_table(id, INT,
+CREATE TABLE IF NOT EXISTS second_table(id INT,
 name VARCHAR(256), score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('1', "John", '10');
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES ('2', "Alex", '3');
