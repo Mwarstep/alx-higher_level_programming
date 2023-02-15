a7fc240e107097c3c2684c354be52c84b4aabeff
Mwarstep <mvoi811@gmail.com>
2023-02-15 02:24:18 -0800

    Task 17 - Correction

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,8 +1,8 @@
 -- Will use the hbtn_0d_tvshows DB to list all genres not linked to Dexter
--- Results soreted in Ascending order by genre name
+-- Results sorted in Ascending order by genre name
 SELECT DISTINCT `name`
   FROM `tv_genres` AS g
-	INNER JOIN `tv_shows_genres` AS s
+	INNER JOIN `tv_show_genres` AS s
 	ON g.`id` = s.`genre_id`
 
 	INNER JOIN `tv_shows` AS t
