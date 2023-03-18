d638311ecf28dacf34b2e1defae37afd5c254701
Mwarstep <mvoi811@gmail.com>
2023-03-17 21:50:52 -0700

    Task 6 - New Model

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Containts Base and State class, the intstances of delarative_base()
+"""
+from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+
+mymetadata = MetaData()
+Base = declarative_base(metadata=mymetadata)
+
+class State(base):
+    """
+    Class with ID and Name attributes of each state
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name =  Column(String(128), nullable=False)
