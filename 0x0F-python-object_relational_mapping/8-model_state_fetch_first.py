cd49294f22509885996d2382d4c4b189fb198b66
Mwarstep <mvoi811@gmail.com>
2023-03-17 22:33:55 -0700

    Task 8 correction

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).frirst()
+    instance = session.query(State).first()
     if instance is None:
         print("Nothing")
     else:
