ce219850384823e3a22060da6d166d49ee150ada
Mwarstep <mvoi811@gmail.com>
2023-03-31 06:20:36 -0700

    Test v.3

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Lists 10 commits, from the most recent to oldest, in a repository 
+of a user
+"""
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    url = 'https://api.github.com'
+    url_2 = '{0}/repos/{1}/{2}/commits'.format(url, argv[2], argv[1])
+    req = requests.get(url_2).json()
+
+    for com in req[0:10]:
+        print(com['sha'] + ':', com['commit']['author']['name'])
