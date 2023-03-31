51b2deb35eb31dec30db95742e39e4571341d7ef
Mwarstep <mvoi811@gmail.com>
2023-03-31 03:25:51 -0700

    Task 1 - 5 Test v.1

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Takes a URL, sends a request and displays the value of the var
+X-Request-Id in the response header
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    req = requests.get(url)
+    print(req.headers.get('X-Request-Id'))
