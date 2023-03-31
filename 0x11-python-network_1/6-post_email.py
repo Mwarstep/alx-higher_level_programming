dffca09700417923009e81bf49bdf0ebc4c946cb
Mwarstep <mvoi811@gmail.com>
2023-03-31 06:01:45 -0700

    Task 6 - 9 Test v.1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Takes a URL and email address, sends a POST request to passed URL with
+the email as a parameter and finally displays the body of the response
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    val = {'email': sys.argv[2]}
+
+    req = requests.post(url, data=val)
+    print(req.text)
