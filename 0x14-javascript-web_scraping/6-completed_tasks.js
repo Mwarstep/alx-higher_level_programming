b793a7b1ec5ba09e4b51d1766b9ccbcdc39cd901
Mwarstep <mvoi811@gmail.com>
2023-04-25 05:08:09 -0700

    Task 6 - Tested v.1

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+const completedTasksByUser = {};
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const tasks = JSON.parse(body);
+
+  tasks.forEach(task => {
+    if (task.completed) {
+      completedTasksByUser[task.userId] = (completedTasksByUser[task.userId] || 0) + 1;
+    }
+  });
+
+  Object.keys(completedTasksByUser).forEach(userId => {
+  console.log(`${userId}: ${completedTasksByUser[userId]}`);
+  });
+});
