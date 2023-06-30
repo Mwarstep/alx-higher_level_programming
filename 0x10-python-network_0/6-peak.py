42bee28eb101a8a2dd82425ffe8ebb3681d76c9b
Mwarstep <mvoi811@gmail.com>
2023-06-30 04:08:08 -0700

    Find a Peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Will find a peak in the list of Integers."""
+
+
+def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers(int) - The list of integers.
+    Returns: The peak of list_of_integers or None.
+    """
+    if not list_of_integers:
+        return None
+
+    low = 0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] < list_of_integers[mid + 1]:
+            low = mid + 1
+        else:
+            high = mid
+
+    return list_of_integers[low]
