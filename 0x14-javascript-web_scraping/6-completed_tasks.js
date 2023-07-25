5264d8cc92407c74cd8628b9597ca82d86a22fd2
Mwarstep <mvoi811@gmail.com>
2023-07-25 07:07:53 -0700

    Update v.3

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,17 +3,20 @@
 const request = require('request');
 
 request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    const resp = {};
-    const json = JSON.parse(body);
-    for (let x = 0; x < json.length; x++) {
-      if (json[x].completed === true) {
-        if (resp[json[x].userId] === undefined) {
-          resp[json[x].userId] = 0;
-        }
-        resp[json[x].userId]++;
-      }
-    }
-    console.log(resp);
+  if (err) {
+    console.error('Error:', err.message);
+    return;
   }
+
+  const resp = {};
+  const json = JSON.parse(body);
+  for (let x = 0; x < json.length; x++) {
+    if (json[x].completed === true) {
+      if (resp[json[x].userId] === undefined) {
+        resp[json[x].userId] = 0;
+      }
+      resp[json[x].userId]++;
+    }
+  }
+  console.log(resp);
 });
